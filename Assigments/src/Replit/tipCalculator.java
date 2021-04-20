dc18672507210d9f609cda84961b5e47bc2b2104
Benjamin <bnymnpinar@gmail.com>
2021-04-20 17:25:30 +0300

    tip calculator

--- Assigments/src/Replit/tipCalculator.java
+++ Assigments/src/Replit/tipCalculator.java
@@ -26,20 +26,19 @@ public class tipCalculator {
 
 		System.out.println("Number of people entered: " + peopleConverter(numberOfPeople));
 
-		System.out.println("Total to pay: " + getTotalPay(split,quality, checkAmount, totalTip));
+		System.out.println("Total to pay: " + getTotalPay(split, quality, checkAmount, totalTip));
 
-		System.out.println("Total tip: " + getTotalTip(split,quality, checkAmount, totalTip));
-		
-		if(split.equalsIgnoreCase("yes")) {
-			System.out.println("Total per person: " + getTotalPay(split,quality, checkAmount, totalTip) / numberOfPeople);
+		System.out.println("Total tip: " + getTotalTip(split, quality, checkAmount, totalTip));
 
-			System.out.println("Tip per person: " + getTotalTip(split,quality, checkAmount, totalTip) / numberOfPeople);
+		if (split.equalsIgnoreCase("yes")) {
+			System.out.println(
+					"Total per person: " + getTotalPay(split, quality, checkAmount, totalTip) / numberOfPeople);
+
+			System.out
+					.println("Tip per person: " + getTotalTip(split, quality, checkAmount, totalTip) / numberOfPeople);
 
 		}
 
-		
-		
-		
 	}
 
 	public static String peopleConverter(int numberOfPeople) {
@@ -52,48 +51,46 @@ public class tipCalculator {
 	}
 
 	public static double getTotalPay(String split, String quality, double checkAmount, double totalTip) {
-		
-			if (quality.equalsIgnoreCase("poor")) {
-				totalTip = checkAmount * 0.05;
 
-			} else if (quality.equalsIgnoreCase("fair")) {
-				totalTip = checkAmount * 0.1;
+		if (quality.equalsIgnoreCase("poor")) {
+			totalTip = checkAmount * 0.05;
 
-			} else if (quality.equalsIgnoreCase("good")) {
-				totalTip = checkAmount * 0.15;
+		} else if (quality.equalsIgnoreCase("fair")) {
+			totalTip = checkAmount * 0.1;
 
-			} else if (quality.equalsIgnoreCase("great")) {
-				totalTip = checkAmount * 0.2;
+		} else if (quality.equalsIgnoreCase("good")) {
+			totalTip = checkAmount * 0.15;
 
-			} else if (quality.equalsIgnoreCase("excellent")) {
-				totalTip = checkAmount * 0.25;
+		} else if (quality.equalsIgnoreCase("great")) {
+			totalTip = checkAmount * 0.2;
 
-			}
-		
+		} else if (quality.equalsIgnoreCase("excellent")) {
+			totalTip = checkAmount * 0.25;
+
+		}
 
 		return checkAmount + totalTip;
 
 	}
 
 	public static double getTotalTip(String split, String quality, double checkAmount, double totalTip) {
-		
-			if (quality.equalsIgnoreCase("poor")) {
-				totalTip = checkAmount * 0.05;
 
-			} else if (quality.equalsIgnoreCase("fair")) {
-				totalTip = checkAmount * 0.1;
+		if (quality.equalsIgnoreCase("poor")) {
+			totalTip = checkAmount * 0.05;
 
-			} else if (quality.equalsIgnoreCase("good")) {
-				totalTip = checkAmount * 0.15;
+		} else if (quality.equalsIgnoreCase("fair")) {
+			totalTip = checkAmount * 0.1;
 
-			} else if (quality.equalsIgnoreCase("great")) {
-				totalTip = checkAmount * 0.2;
+		} else if (quality.equalsIgnoreCase("good")) {
+			totalTip = checkAmount * 0.15;
 
-			} else if (quality.equalsIgnoreCase("excellent")) {
-				totalTip = checkAmount * 0.25;
+		} else if (quality.equalsIgnoreCase("great")) {
+			totalTip = checkAmount * 0.2;
 
-			}
-		
+		} else if (quality.equalsIgnoreCase("excellent")) {
+			totalTip = checkAmount * 0.25;
+
+		}
 
 		return totalTip;
 
