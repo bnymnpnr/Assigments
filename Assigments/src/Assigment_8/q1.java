d84f27ee8eb41883e375b1d3a465f873341d92a9
Benjamin <bnymnpinar@gmail.com>
2021-04-26 15:26:09 +0300

    Assignment 8 Answers

---
+++ Assigments/src/Assigment_8/q1.java
@@ -0,0 +1,42 @@
+package Assigment_8;
+
+import java.util.Scanner;
+
+public class q1 {
+
+	public static void main(String[] args) {
+		/*
+		 * Question-1
+
+Create a method called "plus", its return is void and it gets no arguments.
+
+It asks the user to input two numbers, then it will add them and print the result.
+Create a scanner within the plus method.
+
+Example:
+
+enter first number:
+1
+enter second number:
+2
+result: 3
+		 */
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter first number:");
+		int first= scan.nextInt();
+		System.out.println("Enter second number:");
+		int second= scan.nextInt();
+		int result;
+		
+		plus(first,second);
+	}
+
+	private static void plus(int a, int b) {
+		int result = a+b;
+		System.out.println("Result is: "+result);
+		
+	}
+
+	
+
+}
