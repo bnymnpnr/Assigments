8fef5bc82e2a9d071b8a83ac304b6de7b9ec3e5f
Benjamin <bnymnpinar@gmail.com>
2021-04-26 16:53:58 +0300

    ans

---
+++ Assigments/src/Assigment_8/q6.java
@@ -0,0 +1,32 @@
+package Assigment_8;
+
+import java.util.Scanner;
+
+public class q6 {
+
+	public static void main(String[] args) {
+		/*
+		 * Question-6
+		 * 
+		 * Create a method called next3 . This method gets an int argument and prints
+		 * the next 3 numbers after that number. Call the method from main method and
+		 * pass num to it.
+		 * 
+		 * enter number 1 next 3 are: 2 3 4
+		 * 
+		 * (put a space between numbers)
+		 */
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		next3(scan.nextInt());
+
+	}
+
+	private static void next3(int number) {
+		for (int i = 0; i < 3; i++) {
+			System.out.print((number + 1 + i) + " ");
+		}
+
+	}
+
+}
