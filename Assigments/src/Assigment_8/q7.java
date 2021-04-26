79255b63648a82b49b54e91a1e2b5816c7c07b49
Benjamin <bnymnpinar@gmail.com>
2021-04-26 18:14:03 +0300

    ans

---
+++ Assigments/src/Assigment_8/q7.java
@@ -0,0 +1,59 @@
+package Assigment_8;
+
+import java.util.Scanner;
+
+public class q7 {
+
+	public static void main(String[] args) {
+		/*
+		 * Question-7
+		 * 
+		 * Complete a method isPalindrome() that will check if the number is a
+		 * palindrome. Print your result as a boolean (true or false). Do not convert
+		 * int into a string! Example: input: 1001 output: true
+		 * 
+		 * Example: input: 1234 output: false
+		 */
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int number = scan.nextInt();
+		isPalindrome(number);
+		
+	}
+
+	private static void isPalindrome(int num) {
+		/*
+		 1234;
+		 
+		 1234%10 = 4*10
+		 1234/10= 123
+		 
+		 123%10=3*10
+		 123/10=12
+		 
+		 12%10 =2*10
+		 12/10=1
+		 
+		 1
+		 
+		 4321
+		 
+		 */
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Assigments/src/Assigment_8/q8.java
@@ -0,0 +1,41 @@
+package Assigment_8;
+
+import java.util.Scanner;
+
+public class q8 {
+
+	public static void main(String[] args) {
+		/*
+		 * Question-8
+		 * 
+		 * Complete a method fib() that will compute Fibonacci numbers In fibonacci
+		 * series, next number is the sum of previous two numbers for example 0, 1, 1,
+		 * 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,……... The first two numbers of fibonacci
+		 * series are 0 and 1. Given a number num, print n-th Fibonacci Number.
+		 * 
+		 * Input : 2 Output : 1
+		 * 
+		 * Input : 9 Output : 21
+		 */
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int count = scan.nextInt();
+		fib(count);
+	}
+
+	private static void fib(int count) {
+		int a = 0;
+		int b = 1;
+		System.out.println("fibonacci series of " + count);
+
+		for (int i = 0; i < count; i++) {
+			int sum = a + b;
+			System.out.print(a + " ");
+
+			a = b;
+			b = sum;
+		}
+
+	}
+
+}
