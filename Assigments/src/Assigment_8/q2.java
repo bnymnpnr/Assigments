c9043d1680eb32033e1a32a54faf8ba7ef1e48d3
Benjamin <bnymnpinar@gmail.com>
2021-04-26 15:35:44 +0300

    answers

---
+++ Assigments/src/Assigment_8/q2.java
@@ -0,0 +1,35 @@
+package Assigment_8;
+
+import java.util.Scanner;
+
+public class q2 {
+
+	public static void main(String[] args) {
+		/*
+		 * Question-2
+		 * 
+		 * Create a method called cube. Write all required code inside this method in
+		 * order to ask the user for a number and then prints the cubed value of that
+		 * number:
+		 * 
+		 * Example:
+		 * 
+		 * input: 5 output: 125
+		 */
+
+		Scanner scan = new Scanner(System.in);
+System.out.println("Enter a number to calculate cube:");
+int number = scan.nextInt();
+cube(number);
+
+	}
+
+	private static void cube(int number) {
+		int result = 1;
+		for (int i = 0; i <3; i++) {
+			result*=number;
+		}
+		System.out.println(result);
+	}
+
+}
