ed5d58f2212e00c00f27121007fc130d44f1d6d2
Benjamin <bnymnpinar@gmail.com>
2021-05-04 18:59:54 +0300

    fizzArray(4) [0, 1, 2, 3]

--- Assigments/src/Lab_4/q23.java
+++ Assigments/src/Lab_4/q23.java
@@ -9,6 +9,7 @@ public class q23 {
 	 * fizzArray(4) [0, 1, 2, 3] fizzArray(1) [0] fizzArray(10) [0, 1, 2, 3, 4, 5,
 	 * 6, 7, 8, 9]
 	 */
+	
 
 	public static void main(String[] args) {
 		fizzArray(10);
