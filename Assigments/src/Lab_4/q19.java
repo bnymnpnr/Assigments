ba1d9658ebb8002d086c3ad60e0833a1e6ff29bd
Benjamin <bnymnpinar@gmail.com>
2021-05-05 01:32:58 +0300

    every 6 will be followed by at least one 7

---
+++ Assigments/src/Lab_4/q19.java
@@ -0,0 +1,38 @@
+package Lab_4;
+
+import java.util.Arrays;
+
+public class q19 {
+
+	public static void main(String[] args) {
+		// q19 Write a method that accepts an array and prints the total of the numbers
+		// in the array, except ignore sections of numbers starting with a 6 and extending to
+		// the next 7 (every 6 will be followed by at least one 7)
+
+		int[] x = { 1, 2, 2, 6, 99, 99, 7, 3};
+
+		int sum = 0;		
+		
+		boolean boolCheckSix = false;	
+		for (int i = 0; i < x.length; i++) {
+
+			if (boolCheckSix) {
+
+				if (x[i] == 7) {
+
+					boolCheckSix = false;
+				}
+
+			} else if (x[i] == 6) {
+
+				boolCheckSix = true;
+
+			} else {
+
+				sum = sum + x[i];
+			}
+		}
+		System.out.println(sum);
+	}
+
+}
