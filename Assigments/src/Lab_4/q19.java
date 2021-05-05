fd08614bd09a9ee7a2569e4aec5cdb531dd00a4c
Benjamin <bnymnpinar@gmail.com>
2021-05-05 20:03:17 +0300

    every 6 will be followed by at least one 7

--- Assigments/src/Lab_4/q19.java
+++ Assigments/src/Lab_4/q19.java
@@ -6,33 +6,64 @@ public class q19 {
 
 	public static void main(String[] args) {
 		// q19 Write a method that accepts an array and prints the total of the numbers
-		// in the array, except ignore sections of numbers starting with a 6 and extending to
+		// in the array, except ignore sections of numbers starting with a 6 and
+		// extending to
 		// the next 7 (every 6 will be followed by at least one 7)
 
-		int[] x = { 1, 2, 2, 6, 99, 99, 7, 3};
-
-		int sum = 0;		
+		int[] x = { 1, 2, 2, 6, 99, 99, 7 };
 		
-		boolean boolCheckSix = false;	
+		int sum = 0;
+		int indexOf6 = 0;
+		int indexOf7 = 0;
+
 		for (int i = 0; i < x.length; i++) {
-
-			if (boolCheckSix) {
-
-				if (x[i] == 7) {
-
-					boolCheckSix = false;
-				}
-
-			} else if (x[i] == 6) {
-
-				boolCheckSix = true;
-
+			if (!(x[i] == 6)) {
+				sum += x[i];
 			} else {
-
-				sum = sum + x[i];
+				indexOf6 = i;
+				break;
 			}
 		}
+
+		for (int i = indexOf6; i < x.length; i++) {
+			if (x[i] == 7) {
+				indexOf7 = i;
+				break;
+			}
+		}
+
+		if (indexOf7 != 0) {
+			for (int i = indexOf7 + 1; i < x.length; i++) {
+				sum += x[i];
+			}
+
+		}
+
 		System.out.println(sum);
+		
+
+//		int sum = 0;		
+//		
+//		boolean boolCheckSix = false;	
+//		for (int i = 0; i < x.length; i++) {
+//
+//			if (boolCheckSix) {
+//
+//				if (x[i] == 7) {
+//
+//					boolCheckSix = false;
+//				}
+//
+//			} else if (x[i] == 6) {
+//
+//				boolCheckSix = true;
+//
+//			} else {
+//
+//				sum = sum + x[i];
+//			}
+//		}
+//		System.out.println(sum);
 	}
 
 }
